15f10e79eb0b46923c15d576a016064683f84023
jungyoon00 <jy.main.tool@gmail.com>
2023-08-21 20:26:34 +0900

    clear

---
+++ detect.py
@@ -0,0 +1,67 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+max_num_hands = 1
+
+# MediaPipe hands model
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+hands = mp_hands.Hands(
+    max_num_hands=max_num_hands,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5)
+
+cap = cv2.VideoCapture(0)
+f = open("angles.txt", "a")
+
+while cap.isOpened():
+    ret, img = cap.read()
+    if not ret:
+        continue
+
+    img = cv2.flip(img, 1)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    result = hands.process(img)
+
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    if result.multi_hand_landmarks is not None:
+        rps_result = []
+
+        for res in result.multi_hand_landmarks:
+            joint = np.zeros((21, 3))
+            for j, lm in enumerate(res.landmark):
+                joint[j] = [lm.x, lm.y, lm.z]
+            
+            # Compute angles between joints
+            v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19],:] # Parent joint
+            v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],:] # Child joint
+            v = v2 - v1 # [20,3]
+
+            # Normalize v
+            v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+
+            # Get angle using arcos of dot product
+            angle = np.arccos(np.einsum('nt,nt->n',
+                v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:],
+                v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]
+            
+            angle = np.degrees(angle) # Convert radian to degree
+            #print(angle)
+
+            # Inference gesture
+            data = np.array([angle], dtype=np.float64)
+            print(data)
+            
+            mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
+            
+            # Who wins?
+            if len(rps_result):
+                cv2.putText(img, org=(int(img.shape[1] / 2), 100), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=2, color=(0, 0, 255), thickness=3)
+
+    cv2.imshow('Game', img)
+    if cv2.waitKey(1) == ord('q'):
+        f.close()
+        break
